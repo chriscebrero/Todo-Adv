c19f77b474d9c195babcfa1de51ba01dccb7df52
Chris Cebrero <chris.cebrero@gmail.com>
2018-06-17 21:27:07 +0000

    firrst route

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var express = require("express"),
     app = express()
     
+app.get('/', function(req, res){
+    res.send("Hi from Express")
+});
+    
 app.listen(process.env.PORT, function(){
     console.log("APP is running on port " + process.env.PORT);
 });
