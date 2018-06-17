4d02181f3c3701452b3690bdf18fd709d1f1e888
Chris Cebrero <chris.cebrero@gmail.com>
2018-06-17 22:17:25 +0000

    added routes

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 var express = require("express"),
     app = express()
     
+var todoRoutes = require('./routes/todos')    
+    
 app.get('/', function(req, res){
-    res.send("Hi from Express")
+    res.send("hello from root route")
 });
+
+app.use('/api/todos', todoRoutes);
     
 app.listen(process.env.PORT, function(){
     console.log("APP is running on port " + process.env.PORT);
