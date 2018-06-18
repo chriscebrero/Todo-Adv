50d53750ea573e677ed6dcd8b61eb86a1265e731
Chris Cebrero <chris.cebrero@gmail.com>
2018-06-18 00:16:54 +0000

    added css

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ var todoRoutes = require("./routes/todos");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/views'));
+
     
 app.get('/', function(req, res){
     res.sendFile("index.html");
