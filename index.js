ea88cf10b687bdcacc54f04897e2cab688762233
Chris Cebrero <chris.cebrero@gmail.com>
2018-06-18 00:10:58 +0000

    index page

--- index.js
+++ index.js
@@ -7,9 +7,10 @@ var todoRoutes = require("./routes/todos");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/views'));
     
 app.get('/', function(req, res){
-    res.send("hello from root route")
+    res.sendFile("index.html");
 });
 
 app.use('/api/todos', todoRoutes);
